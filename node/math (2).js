9a3ecb1e29dc3f3ffd270620a0e9278aa4e649c0
Lchamodya <73007235+Lchamodya@users.noreply.github.com>
2023-12-02 15:05:13 +0530

    jhv

---
+++ node/math (2).js
@@ -0,0 +1,96 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
+const math = require('./arith');
+
+const n1 = 10;
+const n2 = 20;
+
+//console.log(`Sum is ${math.add(n1, n2)}`);
+
+const mydb = new sqlite3.Database('campus.db', (err) => {
+    if(err){
+        console.error('Error starting db '+err.message);
+    }
+    else {
+        mydb.run(`
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                country TEXT
+            )`
+        );
+    }
+});
+
+const myapp = express();
+
+myapp.set('view engine', 'ejs');
+myapp.set('views', 'views');
+myapp.use(express.static('public'));
+myapp.use(bodyParser.urlencoded({ extended: true }));
+
+myapp.post('/calc/add', (req,res) => {
+    res.json({
+        'msg' : 'This is post route'
+    });
+});
+
+myapp.get('/', (req,res) =>{
+    res.send('<html><body><h1>Hello '+req.query.name+'!</h1></body></html>');
+});
+
+myapp.get('/students/199', (req,res) => {
+    res.render('student', { 
+        fullName: 'Barak Obama',
+        country: 'US',
+        status: 'Retired'
+    });
+});
+
+myapp.get('/register', (req,res) => {
+    res.render('signup');
+});
+
+myapp.post('/signup', (req,res) => {
+//    console.log(req.body); 
+   const { name, country } = req.body; 
+   mydb.run('INSERT INTO users (name, country) VALUES (?, ?)', [name,country], (err, country) => {
+        if (err) {
+        // console.error('Error inserting data into the database:', err.message);
+            res.send('Error submitting the form. '+err.message);
+        } else {
+            // console.log('Data inserted successfully');
+            // res.send('Form submitted successfully!');
+            console.log(country);
+            var nextForm = 'europe.html'; 
+            if(country == 'China') { 
+                nextForm = 'asia.html'; 
+            }
+
+            res.json({
+                status: true,
+                message : 'Registration is confirmed!',
+                redirect : nextForm
+            });
+        }
+    });
+});
+
+myapp.delete('/calc/add', (req,res) => {
+    res.json({
+        'msg' : 'This is del route'
+    });
+});
+
+myapp.get('/calc/add', (req, res) => {
+    //console.log(req);
+    res.json({
+        'out': math.add(parseFloat(req.query.a), parseFloat(req.query.b))
+    });
+});
+
+const port = 3030;
+myapp.listen(port, () => {
+    console.log('Calc serve started');
+});
